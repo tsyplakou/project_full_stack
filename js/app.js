678c7fa68697a9c06b22082ee327cd9ffb9bb3b0
Egor Tsyplakov <egor.tsyplakov@solbeg.com>
2021-01-29 15:37:30 +0300

    Task2: Add script for creating and destroying zombies.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+const iconsPath = "icons/"
+
 document.addEventListener('DOMContentLoaded', () => {
-    // TODO
-});
+    for (let i = 0; i < zombies.length; i++) {
+
+        const zombieElement = document.createElement("img")
+        zombieElement.addEventListener('click', () => {
+            zombieElement.src = iconsPath + "zombie-dead.gif"
+        })
+        let zombieImgName
+
+        if (zombies[i].type === ZOMBIE_TYPE.SMALL){
+            zombieImgName = "zombie-small.gif"
+        }
+        else if (zombies[i].type === ZOMBIE_TYPE.MAD){
+            zombieImgName = "zombie-mad.gif"
+        }
+        else if (zombies[i].type === ZOMBIE_TYPE.STRONG){
+            zombieImgName = "zombie-strong.gif"
+        }
+
+        zombieElement.src = iconsPath + zombieImgName
+        document.querySelector(".zombies").appendChild(zombieElement)
+    }
+})
