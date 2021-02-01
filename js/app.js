9f51daf96ca33c6d8cbae3a04c81119e355e8563
Egor Tsyplakov <egor.tsyplakov@solbeg.com>
2021-02-01 17:39:15 +0300

    Task2: Make zombies inline. Add HP counter. Add timer for zombies killing.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let HP = 100
+let timer
+
 document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < zombies.length; i++) {
 
@@ -11,18 +14,29 @@ document.addEventListener('DOMContentLoaded', () => {
             else {
                 zombieElement.classList.add("dead")
             }
+            setTimeout(() => {
+                zombieElement.remove()
+            }, 3000)
         })
 
         if (zombies[i].type === ZOMBIE_TYPE.SMALL){
             zombieElement.classList.add("small")
+            timer = zombies[i].timer
         }
         else if (zombies[i].type === ZOMBIE_TYPE.MAD){
             zombieElement.classList.add("mad")
+            timer = zombies[i].timer
         }
         else if (zombies[i].type === ZOMBIE_TYPE.STRONG){
             zombieElement.classList.add("strong")
+            timer = zombies[i].timer
         }
 
         document.querySelector(".line3").appendChild(zombieElement)
+        // Destroy zombie object if it near house
+        setTimeout(() => {
+            zombieElement.remove()
+            HP--
+        }, timer)
     }
 })
