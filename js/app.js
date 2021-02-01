48542a383460a3a63f1478a4817b533108b23d45
Egor Tsyplakov <egor.tsyplakov@solbeg.com>
2021-02-01 19:41:13 +0300

    Task3: Add HP progress bar to zombie. Add winner/looser backgrounds.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,71 @@
-let playerHP = 100
+let playerHP = 3
 let timer
 
-document.addEventListener('DOMContentLoaded', () => {
-    for (let i = 0; i < zombies.length; i++) {
 
-        const zombieElement = document.createElement("div")
-        const zombieStatusElement = document.createElement("div")
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-        zombieElement.classList.add("zombie")
-        zombieStatusElement.innerHTML = "<span>0</span> of <span>0</span>"
 
-        zombieElement.addEventListener('click', () => {
-            if (zombieElement.classList.contains("dead")) {
-                zombieElement.classList.remove("dead")
-            }
-            else {
-                zombieElement.classList.add("dead")
-            }
+zombies.addZombieToLine = function(lineNr) {
+    if (this.length <=0) {
+        const battleground = document.querySelector(".battleground")
+        if (playerHP > 0) {
+            battleground.classList.add("winner")
+        }
+        else {
+            battleground.classList.add("loser")
+        }
+    }
+
+    const zombieElement = document.createElement("div")
+    const zombieStatusElement = document.createElement("progress")
+
+    zombieElement.classList.add("zombie")
+    zombieStatusElement.classList.add("zombie-status")
+    zombieStatusElement.max = zombies[0].health
+    zombieStatusElement.value = zombies[0].health
+
+    if (this[0].type === ZOMBIE_TYPE.SMALL){
+        zombieElement.classList.add("small")
+        timer = this[0].timer
+    }
+    else if (this[0].type === ZOMBIE_TYPE.MAD){
+        zombieElement.classList.add("mad")
+        timer = this[0].timer
+    }
+    else if (this[0].type === ZOMBIE_TYPE.STRONG){
+        zombieElement.classList.add("strong")
+        timer = this[0].timer
+    }
+
+    zombieElement.addEventListener('click', () => {
+        zombieStatusElement.value -= HIT_DAMAGE
+        if (zombieStatusElement.value <= 0 && !zombieElement.classList.contains("dead")) {
+            zombieElement.classList.add("dead")
             setTimeout(() => {
                 zombieElement.remove()
-            }, 3000)
-        })
+                this.shift()
+                this.addZombieToLine(getRandomInt(1, 4))
+            }, 2000)
+        }
+    })
 
-        if (zombies[i].type === ZOMBIE_TYPE.SMALL){
-            zombieElement.classList.add("small")
-            timer = zombies[i].timer
-        }
-        else if (zombies[i].type === ZOMBIE_TYPE.MAD){
-            zombieElement.classList.add("mad")
-            timer = zombies[i].timer
-        }
-        else if (zombies[i].type === ZOMBIE_TYPE.STRONG){
-            zombieElement.classList.add("strong")
-            timer = zombies[i].timer
-        }
-
-        document.querySelector(".line3").appendChild(zombieElement)
-        zombieElement.appendChild(zombieStatusElement)
-        // Destroy zombie object if it near house
-        setTimeout(() => {
+    zombieElement.appendChild(zombieStatusElement)
+    document.querySelector(`.line${lineNr}`).appendChild(zombieElement)
+    // Destroy zombie object if it near house
+    setTimeout(() => {
+        if (!zombieElement.classList.contains("dead")){
             zombieElement.remove()
             playerHP--
-        }, timer)
-    }
+            this.shift()
+            this.addZombieToLine(getRandomInt(1, 4))
+        }
+    }, timer)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    zombies.addZombieToLine(getRandomInt(1, 4))
 })
